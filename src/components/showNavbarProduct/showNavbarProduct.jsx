33d1b7e3cf4ef82715717bc509d733ec5ecb3a7c
nickbelwin <belwalkarsarvesh@gmail.com>
2024-01-03 15:03:34 +0530

    new

--- src/components/showNavbarProduct/showNavbarProduct.jsx
+++ src/components/showNavbarProduct/showNavbarProduct.jsx
@@ -352,7 +352,7 @@ const ShowNavbarProducts = () => {
                     {!loader ? filterProducts?.map((val) => {
                         return (
                           
-                                <div className=" card" onClick={()=>{linkHandler(val._id)}} key={val._id}>
+                                <div className=" text-left card" onClick={()=>{linkHandler(val._id)}} key={val._id}>
                                 
                                     <LazyLoadImage className="image rounded-md" src={val.displayImage} placeholderSrc={"https://www.beyoung.in/beyoung-loader.gif"} />
                                     {/* <img className="image rounded-md" src={val.displayImage} alt="" />  */}
